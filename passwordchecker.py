24cad8530fc5cdbfc8129c55c9c149addc107b76
Usagi Tejima <99480005+Usagitejima@users.noreply.github.com>
2024-09-13 17:35:24 -0600

    Update passwordchecker.py
    Made the algorithm better. The password checker now checks the password validity by requiring at least one of the components of uppercase letter, lowercase letter, number and special character.

--- passwordchecker.py
+++ passwordchecker.py
@@ -12,25 +12,29 @@ def isValid(password):
         for char in password:
             if(char.isdigit()):
                 num = True
-            else:
-                print("At least one number is required. ")
 
             if(char.islower()):
                 lowerCase = True
-            else:
-                print("At least one lowercase letter is required. ")
 
             if(char.isupper()):
                 upperCase = True
-            else:
-                print("At least one uppercase letter is required. ")
 
             if(not char.isalnum()):
                 special = True
-            else:
-                print("At least one special character is required. ")
 
-        return "Valid password. "
+        if (num == False or lowerCase == False or upperCase == False or special == False):
+            if (num == False):
+                print("At least one number is required. ")
+            if(lowerCase == False):
+                print("At least one lowercase letter is required. ")
+            if(upperCase == False):
+                print("At least one uppercase letter is required. ")
+            if(special == False):
+                print("At least one special character is required. ")
+        else:
+            print("Valid password.")
+
+
 
     else:
         return "Password must be between the length of 6 and 14. "
